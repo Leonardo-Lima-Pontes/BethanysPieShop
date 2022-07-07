9dc5300589ed0ebee9cee11e5d5070052e5f6574
Leonardo-Lima-Pontes <bruxodeveloper@hotmail.com>
2022-07-07 14:53:10 -0300

    creation(Database): DbContext, connection string created

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,12 +1,16 @@
+using BethanysPieShop.Database;
 using BethanysPieShop.Repositories;
 using BethanysPieShop.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]));
 
 var app = builder.Build();
 
